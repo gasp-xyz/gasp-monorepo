b6149e39f1a8a952f9caaff4e4d2cf9ed7299614
Mateusz Nowakowski <mat13mn@gmail.com>
2024-02-09 16:14:52 +0100

    rolldown contract & simple tests

---
+++ rolldown-contract/src/token.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestToken is ERC20{
+
+    constructor () ERC20 ("DevToken","DEV"){
+      _mint(msg.sender, 100000000000000000000000000);
+    }
+
+    function mint(address to, uint256 amount) public virtual {
+        _mint(to,amount);
+    }
+
+    function burn(address form, uint amount) public virtual {
+        _burn(form, amount);
+    }
+}
