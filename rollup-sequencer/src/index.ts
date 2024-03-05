a8ba3c116fd165d929c6eadd2b9a621b76a7f326
Michal Dančo <devdanco@gmail.com>
2024-03-05 13:44:32 +0100

    feat: add cancel monitoring (#49)
    * feat: add cancel monitoring
    
    * fix: do nothing when there is no pending request
    
    * fix: order
    
    * chore: update versions
    
    * chore: update versions

--- rollup-sequencer/src/index.ts
+++ rollup-sequencer/src/index.ts
@@ -2,81 +2,119 @@ import { Mangata, signTx } from "@mangata-finance/sdk";
 import "@mangata-finance/types";
 import { Keyring } from "@polkadot/api";
 import "dotenv/config";
-import { createPublicClient, webSocket } from "viem";
-import rolldownAbi from './RollDown.json' assert {type: 'json'};
+import { createPublicClient, encodeAbiParameters, webSocket } from "viem";
+import rolldownAbi from "./RollDown.json" assert { type: "json" };
 
 type ContractAddress = `0x${string}`;
 
-const mangataContractAddress = process.env.MANGATA_CONTRACT_ADDRESS! as ContractAddress;
+const mangataContractAddress = process.env
+	.MANGATA_CONTRACT_ADDRESS! as ContractAddress;
 
 function sleep_ms(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+	return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 async function main() {
-  let abi = rolldownAbi.abi;
-  const publicClient = createPublicClient({
-    transport: webSocket(process.env.ETH_CHAIN_URL, {
-      retryCount: 5,
-    }),
-  });
+	const abi = rolldownAbi.abi;
+	const publicClient = createPublicClient({
+		transport: webSocket(process.env.ETH_CHAIN_URL, {
+			retryCount: 5,
+		}),
+	});
 
-  while (true) {
-    try {
-      const data = await publicClient.readContract({
-        address: mangataContractAddress,
-        abi: abi,
-        functionName: "getUpdateForL2",
-      }) as any;
-      console.log(data)
-      break;
-    } catch (e) {
-      console.log(`${mangataContractAddress} contract not found`)
-      await sleep_ms(1000);
-    }
-  }
+	while (true) {
+		try {
+			const data = (await publicClient.readContract({
+				address: mangataContractAddress,
+				abi: abi,
+				functionName: "getUpdateForL2",
+			})) as any;
+			console.log(data);
+			break;
+		} catch (e) {
+			console.log(`${mangataContractAddress} contract not found`);
+			await sleep_ms(1000);
+		}
+	}
 
-  const api = await Mangata.instance([process.env.MANGATA_NODE_URL!]).api();
+	const api = await Mangata.instance([process.env.MANGATA_NODE_URL!]).api();
 
-  const keyring = new Keyring({ type: "sr25519" });
-  const collator = keyring.addFromUri(process.env.MNEMONIC!)
+	const keyring = new Keyring({ type: "sr25519" });
+	const collator = keyring.addFromUri(process.env.MNEMONIC!);
 
-  await api.derive.chain.subscribeNewHeads(async (header) => {
-    console.log(`block #${header.number} was authored by ${header.author}`);
+	await api.derive.chain.subscribeNewHeads(async (header) => {
+		console.log(`block #${header.number} was authored by ${header.author}`);
 
-    if (header.author?.toString() === collator.address) {
-      const data = await publicClient.readContract({
-        address: mangataContractAddress,
-        abi: abi,
-        functionName: "getUpdateForL2",
-      }) as any;
+		if (header.author?.toString() === collator.address) {
+			const data = (await publicClient.readContract({
+				address: mangataContractAddress,
+				abi: abi,
+				functionName: "getUpdateForL2",
+			})) as any;
 
-      console.log(data)
+			console.log(data);
 
-      data.order = data.order.map((e: any) => {
-        switch (e) {
-          case 0: {
-            return "DEPOSIT";
-          }
-          case 1: {
-            return "WITHDRAWAL";
-          }
-          case 2: {
-            return "CANCEL_RESOLUTION";
-          }
-          case 3: {
-            return "L2_UPDATES_TO_REMOVE";
-          }
-        }
-      });
+			data.order = data.order.map((e: any) => {
+				switch (e) {
+					case 0: {
+						return "DEPOSIT";
+					}
+					case 1: {
+						return "WITHDRAWAL";
+					}
+					case 2: {
+						return "CANCEL_RESOLUTION";
+					}
+					case 3: {
+						return "L2_UPDATES_TO_REMOVE";
+					}
+				}
+			});
 
-      await signTx(api, api.tx.rolldown.updateL2FromL1(data), collator);
-    }
-  });
+			await signTx(api, api.tx.rolldown.updateL2FromL1(data), collator);
+		} else {
+			const events = await api.query.system.events();
+			const pendingRequestsEvents = events.filter(
+				(event) =>
+					event.event.section === "rolldown" &&
+					event.event.method === "PendingRequestStored",
+			);
+
+			if (pendingRequestsEvents.length > 0) {
+				const eventData = pendingRequestsEvents.map((event) =>
+					event.event.data.toPrimitive(),
+				);
+				const requestId = eventData[2]!.toString();
+
+				const data = (await publicClient.readContract({
+					address: mangataContractAddress,
+					abi: abi,
+					functionName: "getUpdateForL2",
+				})) as any;
+
+				// @ts-ignore
+				const encodedData = encodeAbiParameters(
+					abi.find((e: any) => e!.name === "getUpdateForL2")!.outputs!,
+					[data],
+				);
+				const verified = await api.rpc.rolldown.verify_pending_requests(
+					encodedData,
+					requestId,
+				);
+				if (!verified) {
+					await signTx(
+						api,
+						api.tx.rolldown.cancelRequestsFromL1(requestId),
+						collator,
+					);
+				}
+			}
+		}
+	});
 }
 
 main()
-  .then(() => {
-    console.log("Success");
-  })
-  .catch((e) => console.error("Something went wrong", e));
+	.then(() => {
+		console.log("Success");
+	})
+	.catch((e) => console.error("Something went wrong", e));
