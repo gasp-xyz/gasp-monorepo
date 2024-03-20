146e0846be86b718fb9347871fdd92e7e884e1fe
stanieltron <sqo.stano@gmail.com>
2024-03-20 17:20:07 +0100

    timestamp an failed deposit handling

--- rolldown-contract/src/rolldown.sol
+++ rolldown-contract/src/rolldown.sol
@@ -39,6 +39,11 @@ contract RollDown {
         address tokenAddress,
         uint256 amount
     );
+    event FundsReturned(
+        address depositRecipient,
+        address tokenAddress,
+        uint256 amount
+    );
     event cancelAndCalculatedHash(bytes32 cancelHash, bytes32 calculatedHash);
 
     enum Origin {
@@ -56,27 +61,27 @@ contract RollDown {
         address depositRecipient;
         address tokenAddress;
         uint256 amount;
-        bytes32 blockHash;
+        uint256 timeStamp;
     }
 
     struct L2UpdatesToRemove {
         RequestId requestId;
         uint256[] l2UpdatesToRemove;
-        bytes32 blockHash;
+        uint256 timeStamp;
     }
 
     struct CancelResolution {
         RequestId requestId;
         uint256 l2RequestId;
         bool cancelJustified;
-        bytes32 blockHash;
+        uint256 timeStamp;
     }
 
     struct WithdrawalResolution {
         RequestId requestId;
         uint256 l2RequestId;
         bool status;
-        bytes32 blockHash;
+        uint256 timeStamp;
     }
 
     struct L1Update {
@@ -150,13 +155,13 @@ contract RollDown {
             "Token transfer failed"
         );
 
-        bytes32 blockHash = blockhash(block.number);
+        uint256 timeStamp = block.timestamp;
         Deposit memory depositRequest = Deposit({
             requestId: RequestId({origin: Origin.L1, id: counter++}),
             depositRecipient: depositRecipient,
             tokenAddress: tokenAddress,
             amount: amount,
-            blockHash: blockHash
+            timeStamp: timeStamp
         });
         // Add the new request to the mapping
         deposits[depositRequest.requestId.id] = depositRequest;
@@ -351,7 +356,7 @@ contract RollDown {
 
     function process_l2_update_requests_results(
         RequestResult[] calldata results
-    ) private view returns (uint256[] memory) {
+    ) private returns (uint256[] memory) {
         uint256 updatesToBeRemovedCounter = 0;
         if (results.length == 0) {
             return new uint256[](0);
@@ -376,6 +381,7 @@ contract RollDown {
             }
 
             if (element.updateType == UpdateType.DEPOSIT) {
+                process_l2_update_deposit(element);
                 l2UpdatesToBeRemovedTemp[updatesToBeRemovedCounter++] = (
                     element.originRequestId
                 );
@@ -413,13 +419,13 @@ contract RollDown {
             cancel.range.end
         );
         bytes32 correct_hash = keccak256(abi.encode(pending));
-        bytes32 blockHash = blockhash(block.number);
+        uint256 timeStamp = block.timestamp;
 
         CancelResolution memory resolution = CancelResolution({
             requestId: RequestId({origin: Origin.L1, id: counter++}),
             l2RequestId: cancel.requestId.id,
             cancelJustified: correct_hash == cancel.hash,
-            blockHash: blockHash
+            timeStamp: timeStamp
         });
 
         cancelResolutions[resolution.requestId.id] = resolution;
@@ -434,13 +440,13 @@ contract RollDown {
     ) private {
         IERC20 token = IERC20(withdrawal.tokenAddress);
         bool status = token.balanceOf(address(this)) >= withdrawal.amount;
-        bytes32 blockHash = blockhash(block.number);
+        uint256 timeStamp = block.timestamp;
 
         WithdrawalResolution memory resolution = WithdrawalResolution({
             requestId: RequestId({origin: Origin.L1, id: counter++}),
             l2RequestId: withdrawal.requestId.id,
             status: status,
-            blockHash: blockHash
+            timeStamp: timeStamp
         });
 
         withdrawalResolutions[resolution.requestId.id] = resolution;
@@ -455,6 +461,23 @@ contract RollDown {
                 withdrawal.withdrawalRecipient,
                 withdrawal.tokenAddress,
                 withdrawal.amount
+            );
+        }
+    }
+
+    function process_l2_update_deposit(
+        RequestResult calldata depositResult
+    ) private {
+        if (!depositResult.status) {
+            uint256 requestId = depositResult.requestId.id;
+            Deposit memory theDeposit = deposits[requestId];
+            IERC20 token = IERC20(theDeposit.tokenAddress);
+            token.transfer(theDeposit.depositRecipient, theDeposit.amount);
+
+            emit FundsReturned(
+                theDeposit.depositRecipient,
+                theDeposit.tokenAddress,
+                theDeposit.amount
             );
         }
     }
