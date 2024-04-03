f0d388f6248b9eabf08e10ff54f00a80de96e62f
GonCer <gonza.cerro@gmail.com>
2024-04-03 12:39:09 +0200

    update task finalization & completeness validations

--- avs-finalizer/tests/avs.finalizer.corrupted.test.ts
+++ avs-finalizer/tests/avs.finalizer.corrupted.test.ts
@@ -8,9 +8,8 @@ import {
 import {anvil} from "./ethUtils";
 import {
     getOperatorId,
-    optOut,
-    waitForNoTaskResponded,
-    waitForOperatorRegistered,
+    optOut, waitFor, waitForNo,
+    waitForOperatorRegistered, waitForTaskResponded,
 } from "./operatorUtilities";
 jest.setTimeout(1500000);
 
@@ -18,7 +17,7 @@ let dockerUtils: DockerUtils;
 
 
 describe('Corrupted AVS Finalizer', () => {
-    it('It Responds to tasks, but wont be considered - avs wont store any response - 2 mins', async () => {
+    it('It Responds to tasks, but wont be considered - avs wont store any Completeness - 2 mins', async () => {
         console.info("Starting Corrupted Finalizer");
         dockerUtils = new DockerUtils();
         const transport = webSocket(anvil.rpcUrls.default.http[0] , {
@@ -37,9 +36,11 @@ describe('Corrupted AVS Finalizer', () => {
         await new Promise(r => setTimeout(r, 10000));
 
         console.info("Corrupted - Operator Address: " + POperatorAddress + " Id " + operatorId);
-
-        const noResponse = await waitForNoTaskResponded(publicClient, 120);
-        expect(noResponse).toBe(true);
+        const noCompleted = waitForNo(publicClient, 120, "TaskCompleted");
+        const responded = waitForTaskResponded(publicClient, 1);
+        const [response, isNoCompleted] = await Promise.all([responded, noCompleted]);
+        expect(response.length).toBeGreaterThan(0);
+        expect(isNoCompleted).toBe(true);
 
     });
     afterEach(async () => {
@@ -48,3 +49,35 @@ describe('Corrupted AVS Finalizer', () => {
     });
 });
 
+describe('Non Corrupted AVS Finalizer', () => {
+    it('It Responds to tasks, and completeness - 2 mins', async () => {
+        console.info("Starting Non-Corrupted Finalizer");
+        dockerUtils = new DockerUtils();
+        const transport = webSocket(anvil.rpcUrls.default.http[0] , {
+            retryCount: 5,
+        });
+        const publicClient = createPublicClient({
+            transport,
+            chain: anvil,
+        });
+        const POperatorAddress = waitForOperatorRegistered(publicClient);
+        await dockerUtils.startContainer(dockerUtils.FINALIZER_IMAGE , dockerUtils.finalizerLocalEnvironment);
+        const address = await POperatorAddress;
+        console.info("Started");
+        const operatorId = await getOperatorId(publicClient, address as string);
+        // lets wait for some time, to be sure that the operator fully onboard.
+        await new Promise(r => setTimeout(r, 10000));
+
+        console.info("Operator Address: " + POperatorAddress + " Id " + operatorId);
+        const completed = waitFor(publicClient, 1, "TaskCompleted");
+        const responded = waitForTaskResponded(publicClient, 1);
+        const [response, completedResponse] = await Promise.all([responded, completed]);
+        expect(response.length).toBeGreaterThan(0);
+        expect(completedResponse.length).toBeGreaterThan(0);
+
+    });
+    afterEach(async () => {
+        await optOut(dockerUtils);
+        await dockerUtils.stopContainer();
+    });
+});
