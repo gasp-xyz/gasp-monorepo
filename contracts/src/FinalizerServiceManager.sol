2069b52958fbcdcca0078033af8f82dfa7e6e32e
Marian Vanderka <vanderka.marian@gmail.com>
2024-05-13 16:33:30 +0200

    cherry pick re-registration limit

--- contracts/src/FinalizerServiceManager.sol
+++ contracts/src/FinalizerServiceManager.sol
@@ -12,15 +12,13 @@ import "@eigenlayer-middleware/src/ServiceManagerBase.sol";
 contract FinalizerServiceManager is ServiceManagerBase {
     using BytesLib for bytes;
 
-    IFinalizerTaskManager
-        public immutable taskManager;
+    IFinalizerTaskManager public immutable taskManager;
+    /// @notice The minimum threshold for reccurent registration in blocks
+    uint64 public immutable recurrentRegistrationBlocksLimit;
 
     /// @notice when applied to a function, ensures that the function is only callable by the `taskManager`.
     modifier onlyTaskManager() {
-        require(
-            msg.sender == address(taskManager),
-            "onlyTaskManager: not from finalizer task manager"
-        );
+        require(msg.sender == address(taskManager), "onlyTaskManager: not from finalizer task manager");
         _;
     }
 
@@ -28,12 +26,35 @@ contract FinalizerServiceManager is ServiceManagerBase {
         IAVSDirectory _avsDirectory,
         IRegistryCoordinator _registryCoordinator,
         IStakeRegistry _stakeRegistry,
-        IFinalizerTaskManager _taskManager
+        IFinalizerTaskManager _taskManager,
+        uint64 _recurrentRegistrationBlocksLimit
     ) ServiceManagerBase(_avsDirectory, _registryCoordinator, _stakeRegistry) {
         taskManager = _taskManager;
+        recurrentRegistrationBlocksLimit = _recurrentRegistrationBlocksLimit;
     }
 
     function initialize(address initialOwner) public virtual initializer {
         __ServiceManagerBase_init(initialOwner);
+    }
+
+    function registerOperatorToAVS(
+        address operator,
+        ISignatureUtils.SignatureWithSaltAndExpiry memory operatorSignature
+    ) public override onlyRegistryCoordinator {
+        bytes32 operatorId = _registryCoordinator.getOperatorId(operator);
+        // for each quorum check last stake update block number, if the operator was deregistered, there will be an entry,
+        // operator can register again only after some 'recurrentRegistrationBlocksLimit' amount of blocks passes
+        for (uint8 i = 0; i < _registryCoordinator.quorumCount(); i++) {
+            IStakeRegistry.StakeUpdate memory latestStakeUpdate = _stakeRegistry.getLatestStakeUpdate(operatorId, i);
+            // when stake is zero & last update non zero, the OP was deregistered
+            if (latestStakeUpdate.stake == 0 && latestStakeUpdate.updateBlockNumber != 0) {
+                require(
+                    block.number - latestStakeUpdate.updateBlockNumber > recurrentRegistrationBlocksLimit,
+                    "ServiceManager.registerOperatorToAVS: minimum blocks elapsed limit for recurrent registration not met"
+                );
+            }
+        }
+
+        _avsDirectory.registerOperatorToAVS(operator, operatorSignature);
     }
 }
