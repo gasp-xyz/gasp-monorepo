f7d9ef84caeef4147a82ff63dae1be7a4be28b4b
GonCer <gonza.cerro@gmail.com>
2024-06-06 14:22:04 +0200

    add rpc type

--- avs-finalizer/tests/nodeHelper.ts
+++ avs-finalizer/tests/nodeHelper.ts
@@ -24,8 +24,25 @@ export async function buildApi() {
                             }
                             ],
                             type: 'Vec<u8>'
+                        },
+                    pending_l2_requests_hash:
+                        {
+                            description: "Get the hash from the node",
+                            params: [
+                                {
+                                    name: 'chain',
+                                    type: 'String'
+                                },
+                                {
+                                    name: 'at',
+                                    type: 'Hash',
+                                    isOptional: true
+                                }
+                            ],
+                            type: 'Vec<u8>'
                         }
-                }
+                },
+
         }
     });
 }
