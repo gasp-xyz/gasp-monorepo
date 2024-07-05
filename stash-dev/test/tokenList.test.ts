ef0dd167405bdccf1bc348e6e8a35de661f91bd7
olgakonsta <olgakonstantinov@yahoo.com>
2024-07-05 13:24:14 +0200

    chore: fix test tokens

--- stash-dev/test/tokenList.test.ts
+++ stash-dev/test/tokenList.test.ts
@@ -66,8 +66,8 @@ describe('[Token list]', () => {
   it('should mock the token details endpoint method for MGX token', async () => {
     const expectedResponse: TokenInfoStats = {
       tokenId: '0',
-      tokenName: 'Mangata',
-      symbol: 'MGX',
+      tokenName: 'GASP',
+      symbol: 'GASP',
       priceInUSD: '0.0005226090144381267',
       volume24hInUSD: '867.4535759973365',
       liquidity24hInUSD: '508786.95209646935',
@@ -89,8 +89,8 @@ describe('[Token list]', () => {
   it('should mock the token details endpoint method for KSM token', async () => {
     const expectedResponse: TokenInfoStats = {
       tokenId: '4',
-      tokenName: 'Kusama Native',
-      symbol: 'KSM',
+      tokenName: 'GETH',
+      symbol: 'GETH',
       priceInUSD: '21.54441188086054',
       volume24hInUSD: '6.896015943828684',
       liquidity24hInUSD: '57038.15488204146',
