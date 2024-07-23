25652f29e9de24f901bdbf50a46803ae939ade6d
Misha Kolesnik <tenequm@gmail.com>
2024-07-23 18:53:17 +0100

    feat: remove rollup-testnet references and update them to be referencing rollup-holesky instead MGX-1115 MGX-1302 (#190)
    * feat: remove rollup-testnet references and update them to be referencing rollup-holesky instead
    
    * feat: update shared dns record config and align rollup-holesky configs with actual state
    
    * chore: disable finalizer and aggregator installations on rollup-holesky to correspond their current state in GKE
    
    * chore: temporary disable new migrated from rollup-testnet shared endpoints

--- avs-finalizer/tests/index.js
+++ avs-finalizer/tests/index.js
@@ -1,6 +1,6 @@
 import { Mangata } from "@mangata-finance/sdk";
 const main = async () => {
-    const api = await Mangata.instance(["wss://collator-01-ws-rollup-dev.mangata.online"]).api();
+    const api = await Mangata.instance(["wss://collator-01-ws-rollup-dev.gasp.xyz"]).api();
     console.log(api.isConnected);
 };
 main().then(() => console.log("done"));
