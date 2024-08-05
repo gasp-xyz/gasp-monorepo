214ab401a901ea375410215163e848a199ea93af
Olga Konstantinov <olgakonstantinov@yahoo.com>
2024-08-05 12:37:25 +0200

    chore: better swagger docs

--- stash/src/controller/FaucetController.ts
+++ stash/src/controller/FaucetController.ts
@@ -5,25 +5,46 @@ import { pathFaucetSchema } from '../schema/FaucetSchema.js'
 
 export const captcha = async (req: Request, res: Response) => {
   /*
-  #swagger.tags = ['Faucet']
-  #swagger.summary = 'Request tokens from the faucet.'
-  #swagger.description = "Request tokens from the faucet by providing the recipient address and passing captcha verification."
-  #swagger.parameters['toAddress'] = {
-    in: 'path',
-    description: 'The address to receive the tokens',
-    required: true,
-    type: 'string'
-  }
-  #swagger.parameters['captcha'] = {
-    in: 'path',
-    description: 'Captcha token to verify the request',
-    required: true,
-    type: 'string'
-  }
-  #swagger.responses[200] = {
-     description: 'Successful response with no content'
-  }
-*/
+   #swagger.tags = ['Faucet']
+   #swagger.summary = 'Request tokens from the faucet.'
+   #swagger.description = "Request tokens from the faucet by providing the recipient address and passing captcha verification."
+   #swagger.parameters['toAddress'] = {
+     in: 'path',
+     description: 'The address to receive the tokens (must begin with 0x)',
+     required: true,
+     type: 'string'
+   }
+   #swagger.parameters['captcha'] = {
+     in: 'path',
+     description: 'Captcha token to verify the request',
+     required: true,
+     type: 'string'
+   }
+   #swagger.responses[200] = {
+      description: 'Successful response with no content'
+   }
+   #swagger.responses[403] = {
+      description: 'Forbidden',
+      content: {
+          'application/json': {
+              schema: {
+                  oneOf: [
+                      {
+                          example: {
+                              "message": "Captcha verification failed. Reason: [error-codes]"
+                          }
+                      },
+                      {
+                          example: {
+                              "message": "Address [toAddress] has requested the token more than 3 times."
+                          }
+                      }
+                  ]
+              }
+          }
+      }
+   }
+ */
   try {
     const params = pathFaucetSchema.validateSync(req.params)
     await faucetService.verifyCaptcha(params.captcha)
