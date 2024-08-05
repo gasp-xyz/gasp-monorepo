2ace616a1332b56b1db47cb0eb25ba781576150a
Olga Konstantinov <olgakonstantinov@yahoo.com>
2024-08-05 12:20:28 +0200

    chore: improve swagger docs

--- stash/src/controller/FaucetController.ts
+++ stash/src/controller/FaucetController.ts
@@ -4,6 +4,26 @@ import { Request, Response } from 'express'
 import { pathFaucetSchema } from '../schema/FaucetSchema.js'
 
 export const captcha = async (req: Request, res: Response) => {
+  /*
+  #swagger.tags = ['Faucet']
+  #swagger.summary = 'Request tokens from the faucet.'
+  #swagger.description = "Request tokens from the faucet by providing the recipient address and passing captcha verification."
+  #swagger.parameters['toAddress'] = {
+    in: 'path',
+    description: 'The address to receive the tokens',
+    required: true,
+    type: 'string'
+  }
+  #swagger.parameters['captcha'] = {
+    in: 'path',
+    description: 'Captcha token to verify the request',
+    required: true,
+    type: 'string'
+  }
+  #swagger.responses[200] = {
+     description: 'Successful response with no content'
+  }
+*/
   try {
     const params = pathFaucetSchema.validateSync(req.params)
     await faucetService.verifyCaptcha(params.captcha)
