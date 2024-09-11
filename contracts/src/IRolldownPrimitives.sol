b8eb18cb9c52447ccbc959f720b209a8081df656
mateuszaaa <mateusz.cz.nowakowski@gmail.com>
2024-09-11 16:05:05 +0200

    add 'L2UpdateAccepted' event (#232)
    * fix - add explicit error for range boundries
    
    * add 'L2UpdateAccepted' event

--- contracts/src/IRolldownPrimitives.sol
+++ contracts/src/IRolldownPrimitives.sol
@@ -15,36 +15,6 @@ interface IRolldownPrimitives {
         bool cancelJustified
     );
 
-    // NOTE: PR DESC
-    // event WithdrawalResolutionAcceptedIntoQueue(
-    //     uint256 requestId,
-    //     bool success
-    // );
- 
-    // NOTE: PR DESC
-    // event L2UpdatesToRemovedAcceptedIntoQueue(
-    //     uint256 requestId,
-    //     uint256[] l2UpdatesToRemove
-    // );
-    // NOTE: PR DESC
-    // event FundsWithdrawn(
-    //     address withdrawRecipient,
-    //     address tokenAddress,
-    //     uint256 amount
-    // );
-    // NOTE: PR DESC
-    // event FundsReturned(
-    //     address depositRecipient,
-    //     address tokenAddress,
-    //     uint256 amount
-    // );
-
-    // NOTE: PR DESC
-    // event cancelAndCalculatedHash(bytes32 cancelHash, bytes32 calculatedHash);
-
-    // NOTE: PR DESC
-    // event EthWithdrawPending(address sender, uint amount);
-    
     event NativeTokensWithdrawn(
       address sender,
       uint256 amount
@@ -59,6 +29,11 @@ interface IRolldownPrimitives {
     event WithdrawalClosed(
       uint256 requestId,
       bytes32 withdrawalHash
+    );
+
+    event L2UpdateAccepted(
+      bytes32 root,
+      Range range
     );
 
     event NewUpdaterSet(address updater);
@@ -86,13 +61,6 @@ interface IRolldownPrimitives {
         uint256 timeStamp;
     }
 
-    // NOTE: PR DESC
-    // struct L2UpdatesToRemove {
-    //     RequestId requestId;
-    //     uint256[] l2UpdatesToRemove;
-    //     uint256 timeStamp;
-    // }
-
     struct CancelResolution {
         RequestId requestId;
         uint256 l2RequestId;
@@ -100,50 +68,19 @@ interface IRolldownPrimitives {
         uint256 timeStamp;
     }
 
-    // NOTE: PR DESC
-    // struct WithdrawalResolution {
-    //     RequestId requestId;
-    //     uint256 l2RequestId;
-    //     bool status;
-    //     uint256 timeStamp;
-    // }
-
 		enum ChainId{ Ethereum, Arbitrum }
 
     struct L1Update {
         ChainId chain;
         Deposit[] pendingDeposits;
         CancelResolution[] pendingCancelResolutions;
-        // NOTE: PR DESC
-        // WithdrawalResolution[] pendingWithdrawalResolutions;
-        // L2UpdatesToRemove[] pendingL2UpdatesToRemove;
     } 
-
-    // NOTE: PR DESC
-    ////TODO: should be renamed to RequestType
-    //enum UpdateType {
-    //    DEPOSIT,
-    //    WITHDRAWAL,
-    //    WITHDRAWAL_RESOLUTION,
-    //    INDEX_UPDATE,
-    //    CANCEL,
-    //    CANCEL_RESOLUTION
-    //}
 
     struct RequestResult {
         RequestId requestId;
         uint256 originRequestId;
-        // NOTE: PR DESC
-        // UpdateType updateType;
         bool status;
     }
-
-    // NOTE: PR DESC
-    // struct L2Update {
-    //     Cancel[] cancels;
-    //     Withdrawal[] withdrawals;
-    //     RequestResult[] results;
-    // }
 
     struct Cancel {
         RequestId requestId;
