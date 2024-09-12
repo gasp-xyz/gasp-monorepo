bd4c37fda267216af64a31f7d537ef0fa751ab97
Olga Konstantinov <olgakonstantinov@yahoo.com>
2024-09-12 09:20:44 +0200

    Feature/MGX-1223/deposit transactions tracing (#230)
    * add new variables for tracing
    
    * feat: add and upgrade dependencies
    
    * feat: add encrypted values for tracing
    
    * feat: add contract json for viem clients
    
    * feat: MGX-1223: add logic for tracing deposits
    
    * feat: add documentation for tracing deposits
    
    * feat: add tests for tracing of deposits
    
    * chore: update documentation and add postman collection
    
    * feat: small changes

--- stash/src/connector/RedisConnector.ts
+++ stash/src/connector/RedisConnector.ts
@@ -32,3 +32,10 @@ export const timeseries = new RedisClient(
   parseInt(process.env.TIMESERIES_PORT),
   process.env.TIMESERIES_PASS
 )
+
+export function getTimeseriesUrl(): string {
+  const host = process.env.TIMESERIES_HOST
+  const port = parseInt(process.env.TIMESERIES_PORT)
+  const pass = process.env.TIMESERIES_PASS
+  return `redis://:${pass}@${host}:${port}`
+}
