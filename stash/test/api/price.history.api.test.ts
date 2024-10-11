99f86cb21b65b5cf8caa00e16cd929c10650ac3c
Olga Konstantinov <olgakonstantinov@yahoo.com>
2024-10-11 09:40:06 +0200

    Chore/mgx 1488/stash ids instead of currency (#264)
    * feat: change symbol to id in token/{id}/stats endpoint
    
    * feat: change symbols to ids for tokens and pools
    
    * chore: change recognition of liquidity tokens
    
    * chore: change variable name
    
    * Token IDs instead of symbols (#263)
    
    * align tests to use tokenID instead of symbol
    
    * align service tests with tokenID instead of symbol
    
    * some polishing
    
    * adapt tests to use IDs instead of symbols
    
    * change redis dbs
    
    * align tests with changes
    
    * add token_for_price_id value for tests
    
    * use fresh docker images
    
    * skip test
    
    * address CR comments
    
    * bring docker logs to test logs
    
    * run entry point from curr path
    
    * run only unit tests (#265)
    
    * run only unit tests
    
    * run only unit test on ci
    
    * revert formating back to single quotes
    
    * use single quotes
    
    * chore: cleanup
    
    ---------
    
    Co-authored-by: zaujec <peter.zaujec@gasp.xyz>
    Co-authored-by: GonCer <gonza.cerro@gmail.com>

---
+++ stash/test/api/price.history.api.test.ts
@@ -0,0 +1,49 @@
+import { describe, it, expect } from 'vitest'
+import app from '../../src/app'
+import supertest from 'supertest'
+import { tokenIDs } from './utils'
+
+describe('/GET prices', () => {
+  it('GET /price-history - Schema validation', async () => {
+    await supertest(app)
+      .get(`/price-history/0`)
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toMatchSnapshot('Prices')
+        expect(response.body.error).toBeUndefined()
+        expect(response.body.prices).toBeDefined()
+        expect(response.body.prices).toBeInstanceOf(Array)
+      })
+  })
+
+  it.each(tokenIDs)(
+    'should return prices for supported pools: %s',
+    async (tokenID) => {
+      const response = await supertest(app)
+        .get('/price-history/' + tokenID)
+        .expect(200)
+      expect(response.body).to.have.property('prices')
+      expect(response.body.error).toBeUndefined()
+      expect(response.body.prices).toBeDefined()
+      expect(response.body.prices).toBeInstanceOf(Array)
+    }
+  )
+
+  describe('API errors', () => {
+    it('[MGX-597] - pools should not be returned on prices', async () => {
+      const errorMessage =
+        'this must be one of the following values: 0, 1, 2, 3, 4, 5, 7, 15, 19'
+
+      await supertest(app)
+        .get(`/price-history/0-2?interval=day&days=300`)
+        .expect(500)
+        .then((response) => {
+          const invalidTokenNameResponse = response.body
+          expect(invalidTokenNameResponse.exceptionName).to.contain(
+            'ValidationError'
+          )
+          expect(invalidTokenNameResponse.message).to.contain(errorMessage)
+        })
+    })
+  })
+})
---
+++ stash/test/api/price.history.pair.api.test.ts
@@ -0,0 +1,82 @@
+import { describe, expect, it } from 'vitest'
+import supertest from 'supertest'
+import app from '../../src/app'
+import { MAX_DAYS, MAX_INTERVAL } from './utils'
+import Joi from 'joi'
+
+const pricesSchema = Joi.object({
+  prices: Joi.array().items(
+    Joi.array().items(Joi.number().required(), Joi.string().required())
+  ),
+})
+
+describe('APi tests: price-history/pair', () => {
+  const pair = '0/1'
+  const reversedPair = '1/0'
+  it('GET pair 0/2 validate schema', async () => {
+    const ksmMgx = await supertest(app)
+      .get('/price-history/pair/' + pair)
+      .query({
+        interval: MAX_INTERVAL,
+        days: MAX_DAYS,
+      })
+      .expect(200)
+    const validationResult = pricesSchema.validate(ksmMgx.body)
+    expect(validationResult.error).toBeUndefined()
+  })
+
+  it('GET pair 0/2 returns same as pair 2/0', async () => {
+    const ksmMgx = await supertest(app)
+      .get('/price-history/pair/' + pair)
+      .query({
+        interval: MAX_INTERVAL,
+        days: MAX_DAYS,
+      })
+      .expect(200)
+    const mgxKsm = await supertest(app)
+      .get('/price-history/pair/' + reversedPair)
+      .query({
+        interval: MAX_INTERVAL,
+        days: MAX_DAYS,
+      })
+      .expect(200)
+    expect(ksmMgx.body.prices).toHaveLength(mgxKsm.body.prices.length)
+    const timestamps1 = ksmMgx.body.prices.map((p: any) => p[0])
+    const timestamps2 = mgxKsm.body.prices.map((p: any) => p[0])
+    expect(timestamps1).toStrictEqual(timestamps2)
+  })
+
+  it('GET pools/0/0: pool that does not exist expect empty', async () => {
+    await supertest(app)
+      .get('/price-history/pair/0/0')
+      .query({
+        interval: MAX_INTERVAL,
+        days: MAX_DAYS,
+      })
+      .expect(200)
+      .then((response) => {
+        const poolDoesNotExist = response.body
+        expect(poolDoesNotExist.prices).to.be.empty
+      })
+  })
+})
+
+describe('API Errors: price-history/pair', () => {
+  const errorMessage =
+    'this must be one of the following values: 0, 1, 2, 3, 4, 5, 7, 15, 19'
+
+  it('GET pools/foo: token does not exist Expect validation error', async () => {
+    await supertest(app)
+      .get('/price-history/pair/L1Asset/foo')
+      .query({
+        interval: MAX_INTERVAL,
+        days: MAX_DAYS,
+      })
+      .expect(500)
+      .then((response) => {
+        const fooResponse = response.body
+        expect(fooResponse.exceptionName).to.contain('ValidationError')
+        expect(fooResponse.message).to.contain(errorMessage)
+      })
+  })
+})
