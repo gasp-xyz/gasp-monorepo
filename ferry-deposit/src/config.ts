7ea689c56d134b2b2399150c453a4e0990097d26
mateuszaaa <mateusz.cz.nowakowski@gmail.com>
2024-10-16 18:57:15 +0200

    Ferry deposit service cleanup (#272)
    * Ferry service - ethereum only
    
    * make aware of different chains (ethereum/arbitrum)
    
    * apply lints
    
    * fix minor bugs
    
    * fix unit tests
    
    * throw when not enought funds to cover tx fee
    
    * fix formatting
    
    * rework env variables
    
    * move deposits to separate file
    
    * extract L1Interface
    
    * export L2Interface
    
    * move L1 & L2 interfaces to separate files
    
    * clenup
    
    * add docker ignore
    
    * default configs for ferry
    
    * ferry deposit enabled in docker compose
    
    * rename test files
    
    * add delay parameter
    
    * align tests
    
    * align sequencer to contract changes
    
    * fixing automatic cancellation
    
    * initial commit
    
    * l2 tests
    
    * UT works
    
    * chore: forge init
    
    * more & more tests for l1
    
    * tests passing
    
    * ferryService draft impl
    
    * ferry service works
    
    * fix tests & add fee estimation
    
    * add env file for tests
    
    * add vitest config
    
    * enable ferry withdrawal tests
    
    * align sdk/
    
    * add install dependencies step
    
    * enable all tests
    
    * move tests to e2e
    
    * winston logger
    
    * improved logger
    
    * fix linter
    
    * add local env
    
    * add simple readme
    
    * improve logging
    
    * build ferries images
    
    * adjust ferry-deposit
    
    * enable ferry deposit tests on ci
    
    * refactor l1
    
    * refactor ferry-withdrawal
    
    * adjust docker builders names
    
    * refcator
    
    * add env for tests
    
    * skip test phase for ferry-deposits
    
    * cleanup & introduce logger
    
    * update envs and provide Readme
    
    * adjust configs
    
    * update readme
    
    * disable ferries
    
    * trigger ci

---
+++ ferry-deposit/src/config.ts
@@ -0,0 +1,49 @@
+import rolldownAbi from "./Rolldown.json" assert { type: "json" };
+import { z } from "zod";
+import { hexToU8a } from "@polkadot/util";
+import JSONbig from "json-bigint";
+
+const tokensToTrackSchema = z
+	.tuple([
+		z.string().transform((x) => hexToU8a(x, 160)),
+		z.bigint(),
+		z.bigint(),
+	])
+	.array();
+
+export const appConfigSchema = z.object({
+	MANGATA_CONTRACT_ADDRESS: z.string(),
+	ETH_CHAIN_URL: z.string(),
+	MANGATA_NODE_URL: z.string(),
+	MNEMONIC: z.string(),
+	L1_CHAIN: z.string(),
+	TX_COST: z.bigint(),
+	BLOCK_DELAY: z.bigint(),
+	TOKENS_TO_TRACK: z.string().transform(elem => tokensToTrackSchema.parse(JSONbig({ alwaysParseAsBig: true, useNativeBigInt: true }).parse(elem))),
+  LOG: z.string(),
+});
+
+const configuration = appConfigSchema.parse({
+		MANGATA_CONTRACT_ADDRESS: process.env.MANGATA_CONTRACT_ADDRESS!,
+		ETH_CHAIN_URL: process.env.ETH_CHAIN_URL!,
+		MANGATA_NODE_URL: process.env.MANGATA_NODE_URL!,
+		MNEMONIC: process.env.MNEMONIC!,
+		L1_CHAIN: process.env.L1_CHAIN!,
+		TX_COST: BigInt(process.env.TX_COST!),
+		BLOCK_DELAY: BigInt(process.env.BLOCK_DELAY!),
+    TOKENS_TO_TRACK: process.env.TOKENS_TO_TRACK!,
+    LOG: process.env.LOG!,
+	});
+
+
+export const MANGATA_CONTRACT_ADDRESS = configuration.MANGATA_CONTRACT_ADDRESS as `0x${string}`;
+export const ETH_CHAIN_URL = configuration.ETH_CHAIN_URL;
+export const MANGATA_NODE_URL = configuration.MANGATA_NODE_URL;
+export const MNEMONIC = configuration.MNEMONIC;
+export const L1_CHAIN = configuration.L1_CHAIN;
+export const TX_COST = configuration.TX_COST;
+export const BLOCK_DELAY = configuration.BLOCK_DELAY;
+export const TOKENS_TO_TRACK = configuration.TOKENS_TO_TRACK;
+export const LOG = configuration.LOG;
+
+export const ABI = rolldownAbi.abi;
--- ferry-deposit/src/main.ts
+++ ferry-deposit/src/main.ts
@@ -1,7 +1,7 @@
 import util from "node:util";
 import { Keyring } from "@polkadot/api";
 import type { HeaderExtended } from "@polkadot/api-derive/type/types";
-import { hexToU8a } from "@polkadot/util";
+import { hexToU8a, u8aToHex } from "@polkadot/util";
 import "dotenv/config";
 import { signTx } from "gasp-sdk";
 import "gasp-types";
@@ -10,20 +10,37 @@ import {
 	ETH_CHAIN_URL,
 	MANGATA_CONTRACT_ADDRESS,
 	MANGATA_NODE_URL,
-	MIN_PROFIT,
+	TOKENS_TO_TRACK,
 	MNEMONIC,
 	TX_COST,
-} from "./common/constants.js";
+  LOG,
+} from "./config.js";
 
-import { Ferry } from "./ferry/index.js";
-import { L1Api } from "./l1/index.js";
-import { L2Api, getL1ChainType } from "./l2/index.js";
-import { getApi, isSuccess, print } from "./utils/index.js";
+import { Ferry } from "./Ferry.js";
+import { L1Api } from "./l1/L1Api.js";
+import { L2Api, getL1ChainType } from "./l2/L2Api.js";
+import { getApi, isSuccess, print } from "./utils.js";
+import { logger } from "./logger.js";
 
 async function main() {
 	const api = await getApi(MANGATA_NODE_URL);
 	const l2 = new L2Api(api);
 	const l1 = new L1Api(ETH_CHAIN_URL, BLOCK_DELAY);
+
+  logger.info(`Ferry Deposit`);
+  logger.info(`L1             : ${ETH_CHAIN_URL}`);
+  logger.info(`L1 Block Delay : ${ETH_CHAIN_URL}`);
+  logger.info(`L2             : ${MANGATA_NODE_URL}`);
+  logger.info(`Log level      : ${LOG}`);
+
+	TOKENS_TO_TRACK.forEach((token) => {
+		logger.info(
+			`Token to ferry ${u8aToHex(token[0])} minimal profit : ${
+				token[1]
+			} weight : ${token[2]}`,
+		);
+	});
+
 
 	if (!(await api.isReady)) {
 		throw `Cannot connect to ${MANGATA_NODE_URL}`;
@@ -36,20 +53,20 @@ async function main() {
 		l1,
 		l2,
 		TX_COST,
-		MIN_PROFIT,
+		TOKENS_TO_TRACK,
 	);
 
 	let inProgress = false;
 
 	const unwatch = await api.derive.chain.subscribeFinalizedHeads(
 		async (header: HeaderExtended) => {
-			console.info(`New L2 block: #${header.number}`);
+			logger.info(`New L2 block: #${header.number}`);
 			if (inProgress) {
 				return;
 			}
 
 			if (!(await l1.isRolldownDeployed())) {
-				console.info(
+				logger.info(
 					`rolldown contract ${MANGATA_CONTRACT_ADDRESS} not found yet`,
 				);
 				return;
@@ -59,10 +76,10 @@ async function main() {
 			const pending = await ferry.getPendingDeposits();
 			const profitable = await ferry.rateDeposits(pending);
 
-			console.info(`Found ${profitable.length} proffitable deposits`);
+			logger.info(`Found ${profitable.length} proffitable deposits`);
 			if (profitable.length > 0) {
 				const depositToFerry = profitable[0];
-				console.info(`Ferrying deposit ${util.inspect(depositToFerry)}`);
+				logger.info(`Ferrying deposit ${util.inspect(depositToFerry)}`);
 				if (!(await ferry.hasFundsToCoverTxFee())) {
 					throw new Error(`Not enough funds to cover tx fee`);
 				}
@@ -98,9 +115,8 @@ async function main() {
 
 main()
 	.then(() => {
-		print("Success");
 	})
 	.catch((e) => {
-		console.error("Something went wrong", e);
+		logger.error("Something went wrong", e);
 		process.exit(1);
 	});
