2718e6cd022d0047574ec5b1e7a8a8ed866fae68
Serge <2901744+evercoinx@users.noreply.github.com>
2024-10-23 18:33:35 +0200

    refactor(contracts): update public API of Rolldown contract

--- contracts/src/IRolldown.sol
+++ contracts/src/IRolldown.sol
@@ -1,30 +1,42 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-import "./IRolldownPrimitives.sol";
+import {IRolldownPrimitives} from "./IRolldownPrimitives.sol";
 
 interface IRolldown is IRolldownPrimitives {
-
     function deposit_native() external payable;
+
+    function depositNative() external payable;
+
     function deposit_native(uint256 ferryTip) external payable;
 
+    function depositNative(uint256 ferryTip) external payable;
+
     function deposit(address tokenAddress, uint256 amount) external;
+
     function deposit(address tokenAddress, uint256 amount, uint256 ferryTip) external;
 
     function deposit_erc20(address tokenAddress, uint256 amount) external;
+
+    function depositERC20(address tokenAddress, uint256 amount) external;
+
     function deposit_erc20(address tokenAddress, uint256 amount, uint256 ferryTip) external;
 
-    function getUpdateForL2() external view returns (L1Update memory) ;
+    function depositERC20(address tokenAddress, uint256 amount, uint256 ferryTip) external;
 
-    function update_l1_from_l2(bytes32 merkle_root, Range calldata range ) external;
+    function getUpdateForL2() external view returns (L1Update memory);
+
+    function update_l1_from_l2(bytes32 merkle_root, Range calldata range) external;
+
+    function updateL1FromL2(bytes32 merkle_root, Range calldata range) external;
 
     function close_cancel(Cancel calldata cancel, bytes32 merkle_root, bytes32[] calldata proof) external;
 
+    function closeCancel(Cancel calldata cancel, bytes32 merkle_root, bytes32[] calldata proof) external;
+
     function close_withdrawal(Withdrawal calldata withdrawal, bytes32 merkle_root, bytes32[] calldata proof) external;
 
-    function getPendingRequests(
-        uint256 start,
-        uint256 end
-    ) external view returns (L1Update memory);
+    function closeWithdrawal(Withdrawal calldata withdrawal, bytes32 merkle_root, bytes32[] calldata proof) external;
 
+    function getPendingRequests(uint256 start, uint256 end) external view returns (L1Update memory);
 }
