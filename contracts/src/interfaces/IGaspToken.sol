7a8119053abbf5e61a072570b5901f04a4842684
Serge <2901744+evercoinx@users.noreply.github.com>
2024-12-01 15:39:43 +0100

    feat(contracts): implement GaspToken contract

---
+++ contracts/src/interfaces/IGaspToken.sol
@@ -0,0 +1,46 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity 0.8.13;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IGaspToken is IERC20 {
+    event AllowTransfersSet(bool allowTransfers_);
+
+    event SenderWhitelisted(address indexed sender);
+
+    event RecipientWhitelisted(address indexed recipient);
+
+    event SenderDewhitelisted(address indexed sender);
+
+    event RecipientDewhitelisted(address indexed recipient);
+
+    error ZeroAdmin();
+
+    error ZeroSender();
+
+    error ZeroRecipient();
+
+    error SenderAlreadyWhitelisted(address sender);
+
+    error RecipientAlreadyWhitelisted(address recipient);
+
+    error SenderNotWhitelisted(address sender);
+
+    error RecipientNotWhitelisted(address recipient);
+
+    error OperationForbidden(bytes32 selector);
+
+    function setAllowTransfers(bool allowTransfers_) external;
+
+    function whiltelistSender(address sender) external;
+
+    function whitelistRecipient(address recipient) external;
+
+    function dewhitelistSender(address sender) external;
+
+    function dewhitelistRecipient(address recipient) external;
+
+    function getSenderWhitelist() external view returns (address[] memory);
+
+    function getRecipientWhitelist() external view returns (address[] memory);
+}
