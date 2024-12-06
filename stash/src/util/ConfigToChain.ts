7d33aa2ec5a172a54e7a3b2b4d8c2c342fa74659
Olga Konstantinov <olgakonstantinov@yahoo.com>
2024-12-06 16:19:20 +0100

    chore: GASP-1598: cleanup

--- stash/src/util/ConfigToChain.ts
+++ stash/src/util/ConfigToChain.ts
@@ -2,13 +2,13 @@ import { Chain } from 'viem'
 import { anvil, arbitrumSepolia, base, holesky, localhost } from 'viem/chains'
 
 export const CONFIG_TO_CHAIN = new Map<string, Chain>([
-  ['localhost-arbitrum', anvil], //for localhost
+  ['localhost-arbitrum', anvil],
   ['localhost-ethereum', anvil],
   ['localhost-base', anvil],
-  ['holeskky-ethereum', holesky], //for testnet holesky
-  ['holesky-arbitrum', arbitrumSepolia], //for testnet arbitrumSepolia
-  ['holesky-base', base], //for testnet base
-  ['frontend-arbitrum', localhost], //for frontend (fungible) use this
+  ['holeskky-ethereum', holesky],
+  ['holesky-arbitrum', arbitrumSepolia],
+  ['holesky-base', base],
+  ['frontend-arbitrum', localhost],
   ['frontend-ethereum', localhost],
   ['frontend-base', localhost],
 ])
