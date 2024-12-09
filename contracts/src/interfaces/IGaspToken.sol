9db919cf762b57f6b57d274566159a7b9c582941
Serge <2901744+evercoinx@users.noreply.github.com>
2024-12-09 22:02:20 +0400

    feat(contracts): merge whitelists on GaspToken contract

--- contracts/src/interfaces/IGaspToken.sol
+++ contracts/src/interfaces/IGaspToken.sol
@@ -6,43 +6,27 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IGaspToken is IERC20 {
     event AllowTransfersSet(bool allowTransfers_);
 
-    event SenderWhitelisted(address indexed sender);
+    event AddressWhitelisted(address indexed addr);
 
-    event RecipientWhitelisted(address indexed recipient);
-
-    event SenderDewhitelisted(address indexed sender);
-
-    event RecipientDewhitelisted(address indexed recipient);
+    event AddressDewhitelisted(address indexed addr);
 
     error ZeroL1Council();
 
-    error ZeroSender();
-
-    error ZeroRecipient();
+    error ZeroAddress();
 
     error TransfersAlreadyAllowed();
 
-    error SenderAlreadyWhitelisted(address sender);
+    error AddressAlreadyWhitelisted(address addr);
 
-    error RecipientAlreadyWhitelisted(address recipient);
-
-    error SenderNotWhitelisted(address sender);
-
-    error RecipientNotWhitelisted(address recipient);
+    error AddressNotWhitelisted(address addr);
 
     error OperationForbidden(bytes32 selector);
 
     function setAllowTransfers(bool allowTransfers_) external;
 
-    function whitelistSender(address sender) external;
+    function whitelist(address addr) external;
 
-    function whitelistRecipient(address recipient) external;
+    function dewhitelist(address addr) external;
 
-    function dewhitelistSender(address sender) external;
-
-    function dewhitelistRecipient(address recipient) external;
-
-    function getSenderWhitelist() external view returns (address[] memory);
-
-    function getRecipientWhitelist() external view returns (address[] memory);
+    function getWhitelist() external view returns (address[] memory);
 }
