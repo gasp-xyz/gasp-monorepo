9e1ef93359df33c4fdfbb854338ddbdb9e114eba
iStrike7 <siddiquishoeb.bs@gmail.com>
2024-12-10 01:01:37 +0530

    v2 node down

--- scripts/migrate/testnetv2Tov3/src/migrate.ts
+++ scripts/migrate/testnetv2Tov3/src/migrate.ts
@@ -59,11 +59,11 @@ export async function migrateWithoutTransform( sourceBlockHashAt: BlockHash) {
       );
       let allKeys = [];
       let cont = true;
-      let startKey = undefined;
+      // let startKey = undefined;
 
       // Uncomment and edit this to start from a specific key and blockHash
       let sourceBlockHashAt: BlockHash = sourceApi.createType('BlockHash', '0xb1b909f7eb6c4dd59ecf08643940eeaae9c6d9db05dea9635e9154d5fa9efd4a');
-      // let startKey = "0x99971b5749ac43e0235e41b0d37869188ee7418a6531173d60d1f6a82d8f4d5161fbc00ad44a2d08686b0842589456b6b2bee3a3260d30216434b880633cda2852463c8cb4def25cfda6ef3a00000000";
+      let startKey = "0x99971b5749ac43e0235e41b0d37869188ee7418a6531173d60d1f6a82d8f4d51172ee63239e33e39502de6dfa6f5a5ae7db676f79bbe73c870dad45b053a5b2b238d5dedd9c986912843223804000000";
       
       let keys = await sourceApi.rpc.state.getKeysPaged(key, pageItemCount, startKey, sourceBlockHashAt);
       let loop: number = 0;
