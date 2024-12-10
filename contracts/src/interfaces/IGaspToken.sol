502d35680222666021ea6e72cb5b90aa10c2cc09
Serge <2901744+evercoinx@users.noreply.github.com>
2024-12-10 15:42:26 +0400

    feat(contracts): update initialization of GaspToken contract

--- contracts/src/interfaces/IGaspToken.sol
+++ contracts/src/interfaces/IGaspToken.sol
@@ -6,25 +6,29 @@ import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 interface IGaspToken is IERC20 {
     event AllowTransfersSet(bool allowTransfers_);
 
-    event AddedToWhitelist(address indexed address_);
+    event AddedToWhitelist(address indexed addr);
 
-    event RemoveFromWhitelist(address indexed address_);
+    event RemoveFromWhitelist(address indexed addr);
 
     error ZeroL1Council();
 
-    error ZeroAddress();
+    error ZeroUniswapV3Pool();
+
+    error ZeroRolldown();
+
+    error ZeroWhitelistAddress();
 
     error TransfersAlreadyAllowed();
 
-    error AddressAlreadyWhitelisted(address address_);
+    error AddressAlreadyWhitelisted(address addr);
 
-    error AddressNotWhitelisted(address address_);
+    error AddressNotWhitelisted(address addr);
 
-    error OperationForbidden(bytes32 selector_);
+    error OperationForbidden(bytes32 selector);
 
     function setAllowTransfers(bool allowTransfers_) external;
 
-    function addToWhitelist(address address_) external;
+    function addToWhitelist(address addr) external;
 
-    function removeFromWhitelist(address address_) external;
+    function removeFromWhitelist(address addr) external;
 }
