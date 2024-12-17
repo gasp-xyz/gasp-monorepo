0ad3ae2e6089d1ef7709615a1c01ed8bceccf66c
Mateusz Nowakowski <mat13mn@gmail.com>
2024-12-17 13:33:17 +0100

    persist  recent img version

--- ops/helmfiles/redeploy.sh
+++ ops/helmfiles/redeploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-IMAGE_TAG=4aad4a9c917ed04965be1c39f5b5dfc495e27f2d helmfile sync -e prod --kube-context gke_direct-pixel-353917_europe-west1_gasp-prod
+IMAGE_TAG=78d509568fadbdc665b6a67f43f33d1b86834144 helmfile sync -e prod --kube-context gke_direct-pixel-353917_europe-west1_gasp-prod
