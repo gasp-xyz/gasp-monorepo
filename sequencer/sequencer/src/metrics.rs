8f90655f03572f585b79cbae3e4ca5560c938615
Mateusz Nowakowski <mat13mn@gmail.com>
2025-01-14 02:58:31 +0100

    metrics works

--- sequencer/sequencer/src/metrics.rs
+++ sequencer/sequencer/src/metrics.rs
@@ -2,12 +2,12 @@ use prometheus::{Encoder, TextEncoder};
 use warp::Filter;
 
 pub async fn serve_metrics(port: u16) {
-    let metrics = warp::path!("").map(|| {
+    let metrics = warp::any().map(|| {
         let encoder = TextEncoder::new();
         let metric_families = prometheus::gather();
         let mut buffer = vec![];
         encoder.encode(&metric_families, &mut buffer).expect("metrics available");
         warp::reply::with_header(buffer, warp::http::header::CONTENT_TYPE, encoder.format_type())
     });
-    warp::serve(metrics).run(([127, 0, 0, 1], port)).await;
+    warp::serve(metrics).run(([0, 0, 0, 0], port)).await;
 }
