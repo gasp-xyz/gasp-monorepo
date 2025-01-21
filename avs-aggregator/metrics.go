6c18a204b4daa577ec5c0904c63f8e0a484cdec8
Mateusz Nowakowski <mat13mn@gmail.com>
2025-01-21 12:54:27 +0100

    fix precision

--- avs-aggregator/metrics.go
+++ avs-aggregator/metrics.go
@@ -23,27 +23,27 @@ func recordMetrics(logger sdklogging.Logger, rpc *chainio.EthRpc) {
 			address, err := rpc.Clients.Wallet.SenderAddress(context.Background())
 			if err != nil {
 				logger.Error("Could not get account address", "err", err)
-        time.Sleep(60 * time.Second)
-        continue
+				time.Sleep(60 * time.Second)
+				continue
 			}
 
 			latest, err := rpc.Clients.EthHttpClient.BlockNumber(context.Background())
 			if err != nil {
 				logger.Error("Could not get latest block number", "err", err)
-        time.Sleep(60 * time.Second)
-        continue
+				time.Sleep(60 * time.Second)
+				continue
 			}
-
 
 			account_balance, err := rpc.AvsReader.AvsServiceBindings.EthClient.BalanceAt(context.Background(), address, big.NewInt(int64(latest)))
 			if err != nil {
 				logger.Error("could not fetch account balance", "err", err)
-        time.Sleep(60 * time.Second)
-        continue
+				time.Sleep(60 * time.Second)
+				continue
 			}
 
-			balance.Set(float64(account_balance.Uint64()))
-      time.Sleep(60 * time.Second)
+			balanceF64, _ := account_balance.Float64()
+			balance.Set(balanceF64 / 1_000_000_000_000_000_000)
+			time.Sleep(60 * time.Second)
 		}
 	}()
 }
