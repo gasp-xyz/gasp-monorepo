b0d88e0d9687af4f636b7aa20037e03dccdccf8f
Misha Kolesnik <tenequm@gmail.com>
2025-01-23 15:03:18 +0000

    feat: Update sequencer Base RPC URLs to Infura endpoints on rollup-prod (#413)
    * fix: update sequencer base rpc URLs on rollup-prod to use Infura endpoints
    
    * chore: remove shell script files

--- ops/helmfiles/seq_redeploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-IMAGE_TAG=e82c4b3d9539b81f1cbe48066aed73ba5463fc10 helmfile apply -e prod -lname=sequencer-base -lname=sequencer-base-2 -lname=sequencer-base-3 -lname=sequencer-arb -lname=sequencer-arb-2 -lname=sequencer-arb-3 -lname=sequencer-eth -lname=sequencer-eth-2 -lname=sequencer-eth-3 -e prod --kube-context gke_direct-pixel-353917_europe-west1_gasp-prod
