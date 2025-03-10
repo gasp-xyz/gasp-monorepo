cc5edb5003be2f09ce4d120fee0ba13e0228862b
Serge <2901744+evercoinx@users.noreply.github.com>
2025-03-10 22:37:11 +0400

    refactor(contracts): update imports of some contracts

--- contracts/src/GaspMultiRollupServiceStorage.sol
+++ contracts/src/GaspMultiRollupServiceStorage.sol
@@ -1,37 +1,27 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.9;
+pragma solidity 0.8.13;
 
-import "./IGaspMultiRollupService.sol";
-import "@eigenlayer-middleware/src/libraries/BN254.sol";
+import {BN254} from "@eigenlayer-middleware/src/libraries/BN254.sol";
+import {IGaspMultiRollupService} from "./IGaspMultiRollupService.sol";
 import {IRolldown} from "./IRolldown.sol";
 
 abstract contract GaspMultiRollupServiceStorage is IGaspMultiRollupService {
-
-
     IRolldown public rolldown;
     uint32 public chainRdBatchNonce;
     IRolldown.ChainId public chainId;
-
     address public updater;
-
     bool public stalled;
     bool public allowNonRootInit;
     uint256 public lastOpUpdateBlockTimestamp;
-
     uint32 public latestCompletedRdTaskNumber;
-
     uint32 public latestCompletedOpTaskNumber;
     uint32 public latestCompletedOpTaskCreatedBlock;
-
     bytes public quorumNumbers;
     uint32 public quorumThresholdPercentage;
-    
     mapping(uint8 => uint96) public quorumToStakes;
     mapping(bytes32 => mapping(uint8 => uint96)) public operatorAndQuorumToStakes;
-
     mapping(uint8 => BN254.G1Point) public qourumApk;
     mapping(bytes32 => uint8) public operatorIdQuorumCount;
-
     // Placing it here to be able to upgrade the testnetv3
     uint32 public latestCompletedRdTaskCreatedBlock;
 
