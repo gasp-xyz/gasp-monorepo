dac943cf886ff5fb7a88ca00e3d9b944243c5722
Mateusz Nowakowski <mat13mn@gmail.com>
2025-03-10 09:56:32 +0100

    closer draft

--- sequencer/closer/src/closer.rs
+++ sequencer/closer/src/closer.rs
@@ -1,8 +1,20 @@
-use l1api::L1Api;
+use l1api::L1Interface;
+use l2api::L2Interface;
 
-pub struct Closer;
+pub struct Closer<L1, L2>{
+    l1: L1,
+    l2: L2,
+}
 
-// impl Closer {
-//     fn new(
-// }
+impl<L1, L2> Closer<L1, L2> where
+    L1: L1Interface,
+    L2: L2Interface,
+{
+    pub fn new(l1: L1, l2: L2) -> Self {
+        Closer {
+            l1,
+            l2,
+        }
+    }
+}
 
