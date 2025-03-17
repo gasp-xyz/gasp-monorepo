8267641cf87cfe9208c513c649b74f5c9dfc0599
Serge <2901744+evercoinx@users.noreply.github.com>
2025-03-17 13:14:17 +0400

    refactor(contracts): refactor multistage script

--- contracts/script/MultiStage.s.sol
+++ contracts/script/MultiStage.s.sol
@@ -1,62 +1,26 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.13;
 
-import { EmptyContract } from "@eigenlayer/test/mocks/EmptyContract.sol";
-import { console } from "forge-std/console.sol";
-import { Script } from "forge-std/Script.sol";
-import { stdJson } from "forge-std/StdJson.sol";
-import { Test } from "forge-std/Test.sol";
-import { AnvilSetup } from "./../script/0_AnvilSetup.s.sol";
-import { FinalizerAVSDeployer, FinalizerTaskManager } from "./../script/1_FinalizerAVSDeployer.s.sol";
-import { GaspMultiRollupService, GaspMultiRollupServiceDeployer } from "./../script/GaspMultiRollupServiceDeployer.s.sol";
-import { M2Deployer } from "./../script/M2_Deploy_From_Scratch.s.sol";
-import { IRolldownPrimitives, Rolldown, RolldownDeployer } from "./../script/RolldownDeployer.s.sol";
-import { IRolldown } from "./../src/interfaces/IRolldown.sol";
-import { Utils } from "./utils/Utils.sol";
+import {EmptyContract} from "@eigenlayer/test/mocks/EmptyContract.sol";
+import {console} from "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+import {stdJson} from "forge-std/StdJson.sol";
+import {Test} from "forge-std/Test.sol";
+import {AnvilSetup} from "./../script/0_AnvilSetup.s.sol";
+import {FinalizerAVSDeployer, FinalizerTaskManager} from "./../script/1_FinalizerAVSDeployer.s.sol";
+import {
+    GaspMultiRollupService, GaspMultiRollupServiceDeployer
+} from "./../script/GaspMultiRollupServiceDeployer.s.sol";
+import {M2Deployer} from "./../script/M2_Deploy_From_Scratch.s.sol";
+import {IRolldownPrimitives, Rolldown, RolldownDeployer} from "./../script/RolldownDeployer.s.sol";
+import {IRolldown} from "./../src/interfaces/IRolldown.sol";
+import {Utils} from "./utils/Utils.sol";
 
 contract MultiStage is Script, Utils, Test {
-    function deployRolldown() private {}
-
-    function deploy_rolldown_and_gmrs(IRolldownPrimitives.ChainId chain) internal {
-        Rolldown rolldown;
-        GaspMultiRollupService gmrs;
-        address avsOwner;
-
-        console.log("################################################################################");
-        console.log("Deploying rolldown contracts");
-        console.log("################################################################################");
-        RolldownDeployer rolldownDeployer = new RolldownDeployer();
-        rolldownDeployer.run(chain);
-
-        string memory _CONFIG_PATH = "deploy.config";
-        string memory configData = readConfig(_CONFIG_PATH);
-        avsOwner = stdJson.readAddress(configData, ".permissions.owner");
-
-        console.log("################################################################################");
-        console.log("Deploying gaspMultiRollupService contracts");
-        console.log("################################################################################");
-        GaspMultiRollupServiceDeployer gaspMultiRollupServiceDeployer = new GaspMultiRollupServiceDeployer();
-        gaspMultiRollupServiceDeployer.run(chain);
-
-        string memory _GMRS_OUTPUT_PATH = "gmrs_output";
-        string memory gmrsDeployedContracts = readOutput(_GMRS_OUTPUT_PATH);
-        gmrs = GaspMultiRollupService(stdJson.readAddress(gmrsDeployedContracts, ".addresses.gmrs"));
-
-        string memory _ROLLDOWN_OUTPUT_PATH = "rolldown_output";
-        string memory rolldownDeployedContracts = readOutput(_ROLLDOWN_OUTPUT_PATH);
-        rolldown = Rolldown(payable(stdJson.readAddress(rolldownDeployedContracts, ".addresses.rolldown")));
-
-        vm.startBroadcast(avsOwner);
-
-        gmrs.setRolldown(IRolldown(address(rolldown)));
-        rolldown.setUpdater(address(gmrs));
-
-        vm.stopBroadcast();
-    }
-
     function run() external {
-        string memory variant = vm.envString("ENV_SELECTOR");
-        if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("ethereum-stub"))) {
+        bytes32 env = keccak256(abi.encodePacked(vm.envString("ENV_SELECTOR")));
+
+        if (env == keccak256(abi.encodePacked("ethereum-stub"))) {
             vm.startBroadcast();
             // added some extra call here so nonce does inc
             new EmptyContract();
@@ -88,9 +52,6 @@ contract MultiStage is Script, Utils, Test {
                 console.log("################################################################################");
                 rolldownDeployer.run(IRolldownPrimitives.ChainId.Ethereum);
             } else {
-                //TODO:
-                //redeploy finalizer contracts as well
-
                 console.log("################################################################################");
                 console.log("Deploying rolldown contracts");
                 console.log("################################################################################");
@@ -101,13 +62,11 @@ contract MultiStage is Script, Utils, Test {
             FinalizerTaskManager taskManager;
             address avsOwner;
 
-            string memory _EIGEN_OUTPUT_PATH = "avs_deployment_output";
-            string memory eigenlayerDeployedContracts = readOutput(_EIGEN_OUTPUT_PATH);
+            string memory eigenlayerDeployedContracts = readOutput("avs_deployment_output");
             taskManager =
                 FinalizerTaskManager(stdJson.readAddress(eigenlayerDeployedContracts, ".addresses.taskManager"));
 
-            string memory _ROLLDOWN_OUTPUT_PATH = "rolldown_output";
-            string memory rolldownDeployedContracts = readOutput(_ROLLDOWN_OUTPUT_PATH);
+            string memory rolldownDeployedContracts = readOutput("rolldown_output");
             rolldown = Rolldown(payable(stdJson.readAddress(rolldownDeployedContracts, ".addresses.rolldown")));
 
             string memory _CONFIG_PATH = "deploy.config";
@@ -120,36 +79,35 @@ contract MultiStage is Script, Utils, Test {
             rolldown.setUpdater(address(taskManager));
 
             vm.stopBroadcast();
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("arbitrum-stub"))) {
+        } else if (env == keccak256(abi.encodePacked("arbitrum-stub"))) {
             vm.startBroadcast();
             // added some extra call here so nonce does inc
             new EmptyContract();
             vm.stopBroadcast();
 
-            deploy_rolldown_and_gmrs(IRolldownPrimitives.ChainId.Arbitrum);
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("base-stub"))) {
+            deployRolldownAndGMRS(IRolldownPrimitives.ChainId.Arbitrum);
+        } else if (env == keccak256(abi.encodePacked("base-stub"))) {
             vm.startBroadcast();
             // added some extra call here so nonce does inc
             new EmptyContract();
             vm.stopBroadcast();
 
-            deploy_rolldown_and_gmrs(IRolldownPrimitives.ChainId.Base);
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("base-sepolia"))) {
+            deployRolldownAndGMRS(IRolldownPrimitives.ChainId.Base);
+        } else if (env == keccak256(abi.encodePacked("base-sepolia"))) {
             if (outputExists("base_rolldown_output") || outputExists("arbitrum_gmrs_output")) {
                 revert("Already deployed");
             }
-            deploy_rolldown_and_gmrs(IRolldownPrimitives.ChainId.Base);
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("arbitrum-sepolia"))) {
+            deployRolldownAndGMRS(IRolldownPrimitives.ChainId.Base);
+        } else if (env == keccak256(abi.encodePacked("arbitrum-sepolia"))) {
             if (outputExists("arbitrum_rolldown_output") || outputExists("arbitrum_gmrs_output")) {
                 revert("Already deployed");
             }
-            deploy_rolldown_and_gmrs(IRolldownPrimitives.ChainId.Arbitrum);
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("ethereum-holesky"))) {
+            deployRolldownAndGMRS(IRolldownPrimitives.ChainId.Arbitrum);
+        } else if (env == keccak256(abi.encodePacked("ethereum-holesky"))) {
             console.log("################################################################################");
             console.log("Deploying finalizer contracts");
             console.log("################################################################################");
             FinalizerAVSDeployer finalizerAVSDeployer = new FinalizerAVSDeployer();
-
             if (outputExists(finalizerAVSDeployer.getOutputPath())) {
                 revert("Already deployed");
             }
@@ -166,17 +124,14 @@ contract MultiStage is Script, Utils, Test {
             FinalizerTaskManager taskManager;
             address avsOwner;
 
-            string memory _EIGEN_OUTPUT_PATH = "avs_deployment_output";
-            string memory eigenlayerDeployedContracts = readOutput(_EIGEN_OUTPUT_PATH);
+            string memory eigenlayerDeployedContracts = readOutput("avs_deployment_output");
             taskManager =
                 FinalizerTaskManager(stdJson.readAddress(eigenlayerDeployedContracts, ".addresses.taskManager"));
 
-            string memory _ROLLDOWN_OUTPUT_PATH = "rolldown_output";
-            string memory rolldownDeployedContracts = readOutput(_ROLLDOWN_OUTPUT_PATH);
+            string memory rolldownDeployedContracts = readOutput("rolldown_output");
             rolldown = Rolldown(payable(stdJson.readAddress(rolldownDeployedContracts, ".addresses.rolldown")));
 
-            string memory _CONFIG_PATH = "deploy.config";
-            string memory configData = readConfig(_CONFIG_PATH);
+            string memory configData = readConfig("deploy.config");
             avsOwner = stdJson.readAddress(configData, ".permissions.owner");
 
             vm.startBroadcast(avsOwner);
@@ -185,9 +140,7 @@ contract MultiStage is Script, Utils, Test {
             rolldown.setUpdater(address(taskManager));
 
             vm.stopBroadcast();
-        } else if (
-            keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("upgrade-rolldown-ethereum-holesky"))
-        ) {
+        } else if (env == keccak256(abi.encodePacked("upgrade-rolldown-ethereum-holesky"))) {
             console.log("################################################################################");
             console.log("Upgrading rolldown contracts");
             console.log("################################################################################");
@@ -195,9 +148,7 @@ contract MultiStage is Script, Utils, Test {
             // We only want upgrade and not deploy from scratch
             require(rolldownDeployer.isProxyDeployed(), "Proxy not deployed");
             rolldownDeployer.run(IRolldownPrimitives.ChainId.Ethereum);
-        } else if (
-            keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("upgrade-rolldown-arbitrum-sepolia"))
-        ) {
+        } else if (env == keccak256(abi.encodePacked("upgrade-rolldown-arbitrum-sepolia"))) {
             console.log("################################################################################");
             console.log("Upgrading rolldown contracts");
             console.log("################################################################################");
@@ -205,8 +156,7 @@ contract MultiStage is Script, Utils, Test {
             // We only want upgrade and not deploy from scratch
             require(rolldownDeployer.isProxyDeployed(), "Proxy not deployed");
             rolldownDeployer.run(IRolldownPrimitives.ChainId.Arbitrum);
-        } else if (keccak256(abi.encodePacked(variant)) == keccak256(abi.encodePacked("upgrade-rolldown-base-sepolia")))
-        {
+        } else if (env == keccak256(abi.encodePacked("upgrade-rolldown-base-sepolia"))) {
             console.log("################################################################################");
             console.log("Upgrading rolldown contracts");
             console.log("################################################################################");
@@ -215,7 +165,41 @@ contract MultiStage is Script, Utils, Test {
             require(rolldownDeployer.isProxyDeployed(), "Proxy not deployed");
             rolldownDeployer.run(IRolldownPrimitives.ChainId.Base);
         } else {
-            revert("Unsupported variant");
+            revert("Unsupported environment");
         }
+    }
+
+    function deployRolldownAndGMRS(IRolldownPrimitives.ChainId chain) internal {
+        Rolldown rolldown;
+        GaspMultiRollupService gmrs;
+        address avsOwner;
+
+        console.log("################################################################################");
+        console.log("Deploying rolldown contracts");
+        console.log("################################################################################");
+        RolldownDeployer rolldownDeployer = new RolldownDeployer();
+        rolldownDeployer.run(chain);
+
+        string memory configData = readConfig("deploy.config");
+        avsOwner = stdJson.readAddress(configData, ".permissions.owner");
+
+        console.log("################################################################################");
+        console.log("Deploying gaspMultiRollupService contracts");
+        console.log("################################################################################");
+        GaspMultiRollupServiceDeployer gaspMultiRollupServiceDeployer = new GaspMultiRollupServiceDeployer();
+        gaspMultiRollupServiceDeployer.run(chain);
+
+        string memory gmrsDeployedContracts = readOutput("gmrs_output");
+        gmrs = GaspMultiRollupService(stdJson.readAddress(gmrsDeployedContracts, ".addresses.gmrs"));
+
+        string memory rolldownDeployedContracts = readOutput("rolldown_output");
+        rolldown = Rolldown(payable(stdJson.readAddress(rolldownDeployedContracts, ".addresses.rolldown")));
+
+        vm.startBroadcast(avsOwner);
+
+        gmrs.setRolldown(IRolldown(address(rolldown)));
+        rolldown.setUpdater(address(gmrs));
+
+        vm.stopBroadcast();
     }
 }
