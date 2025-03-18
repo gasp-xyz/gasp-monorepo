7410a3914f050642dbad8a240d38021fd82329c5
Serge Grigorenko <2901744+evercoinx@users.noreply.github.com>
2025-03-18 23:08:43 +0400

    GASP-1573: Contract deployment scripts refactor (#500)
    * feat(contracts): S1_U1MX upgrade (#498) (#499)
    
    * chore: update .releaserc.yaml to use `release/next` as a release branch to allow compatibility with preexisting `release/*` head references
    
    * feat(contracts): S1_U1MX upgrade (#498)
    
    * chore: Update branch-main.yml to watch release/next instead of release branch
    
    * S1_U1MX: EigenLayer Allow Council Avs Ejections  (#445)
    
    changed contract, created script, deployed impl on mainnet eth
    
    Co-authored-by: Misha Kolesnik <tenequm@gmail.com>
    
    ---------
    
    Co-authored-by: Misha Kolesnik <tenequm@gmail.com>
    Co-authored-by: iStrike7 <40371953+iStrike7@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Marian Vanderka <vanderka.marian@gmail.com>
    Co-authored-by: iStrike7 <40371953+iStrike7@users.noreply.github.com>
    
    * refactor(contracts): update imports of some contracts
    
    * chore(contracts): update interfaces structure
    
    * chore(contracts): update libraries structure
    
    * style(contracts): reformat some contracts
    
    * fix(contracts): fix solhint issues
    
    * style(contracts): reformat some contracts
    
    * fix(contracts): fix deployment script configuration
    
    ---------
    
    Co-authored-by: Misha Kolesnik <tenequm@gmail.com>
    Co-authored-by: Marian Vanderka <vanderka.marian@gmail.com>
    Co-authored-by: iStrike7 <40371953+iStrike7@users.noreply.github.com>

--- contracts/src/OperatorStateRetrieverExtended.sol
+++ contracts/src/OperatorStateRetrieverExtended.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.9;
+pragma solidity 0.8.13;
 
-import {IRegistryCoordinator} from "@eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
 import {IBLSApkRegistry} from "@eigenlayer-middleware/src/interfaces/IBLSApkRegistry.sol";
+import {IRegistryCoordinator} from "@eigenlayer-middleware/src/interfaces/IRegistryCoordinator.sol";
 import {IStakeRegistry} from "@eigenlayer-middleware/src/interfaces/IStakeRegistry.sol";
-import {IIndexRegistry} from "@eigenlayer-middleware/src/interfaces/IIndexRegistry.sol";
 import {BitmapUtils} from "@eigenlayer-middleware/src/libraries/BitmapUtils.sol";
 import {OperatorStateRetriever} from "@eigenlayer-middleware/src/OperatorStateRetriever.sol";
 
@@ -15,7 +14,12 @@ import {OperatorStateRetriever} from "@eigenlayer-middleware/src/OperatorStateRe
 contract OperatorStateRetrieverExtended is OperatorStateRetriever {
     using BitmapUtils for *;
 
-    function operatorStakeForQuorum(IRegistryCoordinator registryCoordinator, uint8 quorumNumber, address operator) public virtual view returns (uint96, bool) {
+    function operatorStakeForQuorum(IRegistryCoordinator registryCoordinator, uint8 quorumNumber, address operator)
+        public
+        view
+        virtual
+        returns (uint96, bool)
+    {
         IStakeRegistry stakeRegistry = registryCoordinator.stakeRegistry();
         uint96 weight = stakeRegistry.weightOfOperatorForQuorum(quorumNumber, operator);
         // Return the weight, and `true` if the operator meets the quorum's minimum stake
@@ -23,9 +27,13 @@ contract OperatorStateRetrieverExtended is OperatorStateRetriever {
         return (weight, hasMinimumStake);
     }
 
-    function operatorStakeForQuorumClipped(IRegistryCoordinator registryCoordinator, uint8 quorumNumber, address operator) public virtual view returns (uint96) {
+    function operatorStakeForQuorumClipped(
+        IRegistryCoordinator registryCoordinator,
+        uint8 quorumNumber,
+        address operator
+    ) public view virtual returns (uint96) {
         (uint96 weight, bool hasMinimumStake) = operatorStakeForQuorum(registryCoordinator, quorumNumber, operator);
-        if (!hasMinimumStake){
+        if (!hasMinimumStake) {
             weight = 0;
         }
         return weight;
@@ -44,11 +52,17 @@ contract OperatorStateRetrieverExtended is OperatorStateRetriever {
     //     }
     // }
 
-    function getOperatorsStakesForQuorum(IRegistryCoordinator registryCoordinator, bytes calldata quorumNumbers, address[] calldata operatorsAddr) public virtual view returns (Operator[][] memory){
+    function getOperatorsStakesForQuorum(
+        IRegistryCoordinator registryCoordinator,
+        bytes calldata quorumNumbers,
+        address[] calldata operatorsAddr
+    ) public view virtual returns (Operator[][] memory) {
         IBLSApkRegistry blsApkRegistry = registryCoordinator.blsApkRegistry();
 
         Operator[][] memory operators = new Operator[][](quorumNumbers.length);
-        for (uint256 i = 0; i < quorumNumbers.length; i++) {
+        uint256 quorumNumberCount = quorumNumbers.length;
+
+        for (uint256 i = 0; i < quorumNumberCount; ++i) {
             uint8 quorumNumber = uint8(quorumNumbers[i]);
             bytes memory quorumBytes = new bytes(1);
             quorumBytes[0] = quorumNumbers[i];
@@ -56,34 +70,44 @@ contract OperatorStateRetrieverExtended is OperatorStateRetriever {
             for (uint256 j = 0; j < operatorsAddr.length; j++) {
                 bytes32 operatorId = blsApkRegistry.getOperatorId(operatorsAddr[j]);
                 uint96 weight = operatorStakeForQuorumClipped(registryCoordinator, quorumNumber, operatorsAddr[j]);
-                if (operatorId == 0){
+                if (operatorId == 0) {
                     weight = 0;
                 }
-                if (!BitmapUtils.orderedBytesArrayToBitmap(quorumBytes).isSubsetOf(registryCoordinator.getCurrentQuorumBitmap(operatorId))){
+                if (
+                    !BitmapUtils.orderedBytesArrayToBitmap(quorumBytes).isSubsetOf(
+                        registryCoordinator.getCurrentQuorumBitmap(operatorId)
+                    )
+                ) {
                     weight = 0;
                 }
-                operators[i][j] = Operator({
-                    operator: address(operatorsAddr[j]),
-                    operatorId: operatorId,
-                    stake: weight
-                });
+                operators[i][j] = Operator({operator: address(operatorsAddr[j]), operatorId: operatorId, stake: weight});
             }
         }
-            
+
         return operators;
     }
 
     /// @notice Returns the current quorum bitmap for the given `operatorId` or 0 if the operator is not registered for any quorum
-    function getOperatorIdQuorums(IRegistryCoordinator registryCoordinator, bytes32 operatorId) external view returns (bytes memory) {
+    function getOperatorIdQuorums(IRegistryCoordinator registryCoordinator, bytes32 operatorId)
+        external
+        view
+        returns (bytes memory)
+    {
         uint192 currentBitmap = registryCoordinator.getCurrentQuorumBitmap(operatorId);
         bytes memory quorums = BitmapUtils.bitmapToBytesArray(currentBitmap);
         return quorums;
     }
 
     /// @notice Returns the current quorum bitmap for the given `operatorId` or 0 if the operator is not registered for any quorum
-    function getOperatorsFromIds(IRegistryCoordinator registryCoordinator, bytes32[] calldata operatorIds) external view returns (address[] memory) {
+    function getOperatorsFromIds(IRegistryCoordinator registryCoordinator, bytes32[] calldata operatorIds)
+        external
+        view
+        returns (address[] memory)
+    {
         address[] memory operators = new address[](operatorIds.length);
-        for (uint256 i = 0; i < operatorIds.length; i++) {
+        uint256 operatorIdCount = operatorIds.length;
+
+        for (uint256 i = 0; i < operatorIdCount; ++i) {
             operators[i] = registryCoordinator.getOperatorFromId(operatorIds[i]);
         }
         return operators;
