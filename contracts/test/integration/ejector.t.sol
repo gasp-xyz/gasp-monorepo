a897dfd2180aaf61e07fad6e9cf3696a248cff62
tenequm <tenequm@gmail.com>
2025-03-19 15:10:09 +0000

    chore: fix contracts formatting

--- contracts/test/integration/ejector.t.sol
+++ contracts/test/integration/ejector.t.sol
@@ -3,87 +3,89 @@
 //cd contracts ;  pkill anvil ;  clear ; cd ../ ; make deploy-all-contracts-via-multistage-and-resume  ; cd contracts ;   forge test  -vvv --fork-url=http://localhost:8545 --match-contract 'Integration'
 pragma solidity =0.8.13;
 
-import { FinalizerServiceManager } from "../../src/FinalizerServiceManager.sol";
+import {FinalizerServiceManager} from "../../src/FinalizerServiceManager.sol";
 import {BitmapUtils} from "@eigenlayer-middleware/src/libraries/BitmapUtils.sol";
-import { TestUtils } from "./TestUtils.sol";
+import {TestUtils} from "./TestUtils.sol";
 import "forge-std/StdJson.sol";
 import "forge-std/Test.sol";
 
 contract IntegrationEjectorAdminRuleTest is Test {
+    address deployer = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
 
-   address deployer = address(0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266);
+    function testEjectorCanBeResetByDeployer() public {
+        address tu = address(deployer);
 
-   function testEjectorCanBeResetByDeployer() public {
+        FinalizerServiceManager fsm = FinalizerServiceManager(
+            stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager")
+        );
+        address ejectorAddr = fsm.ejector();
+        emit log_address(ejectorAddr);
 
-         address tu = address(deployer);
+        vm.startBroadcast(tu);
+        fsm.setEjector(tu);
+        vm.stopBroadcast();
 
-         FinalizerServiceManager fsm  = FinalizerServiceManager(stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager"));
-         address ejectorAddr = fsm.ejector();
-         emit log_address(ejectorAddr);
+        address ejectorAddr2 = fsm.ejector();
+        emit log_address(ejectorAddr2);
+        assert(ejectorAddr != ejectorAddr2);
+    }
 
-         vm.startBroadcast(tu);
-         fsm.setEjector(tu);
-         vm.stopBroadcast();
+    function testEjectorCanNotBeResetByaNonDeployer() public {
+        address tu = address(101);
 
-         address ejectorAddr2 = fsm.ejector();
-         emit log_address(ejectorAddr2);
-         assert(ejectorAddr != ejectorAddr2);
+        FinalizerServiceManager fsm = FinalizerServiceManager(
+            stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager")
+        );
+        address ejectorAddr = fsm.ejector();
+        emit log_address(ejectorAddr);
 
-   }
-   function testEjectorCanNotBeResetByaNonDeployer() public {
+        vm.startBroadcast(tu);
+        vm.expectRevert("Ownable: caller is not the owner");
+        fsm.setEjector(tu);
+        vm.stopBroadcast();
 
-         address tu = address(101);
+        address ejectorAddr2 = fsm.ejector();
+        emit log_address(ejectorAddr2);
+        assert(ejectorAddr == ejectorAddr2);
+    }
 
-         FinalizerServiceManager fsm  = FinalizerServiceManager(stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager"));
-         address ejectorAddr = fsm.ejector();
-         emit log_address(ejectorAddr);
+    function testEjectorCanEject() public {
+        FinalizerServiceManager fsm = FinalizerServiceManager(
+            stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager")
+        );
+        address ejectorAddr = fsm.ejector();
 
-         vm.startBroadcast(tu);
-         vm.expectRevert("Ownable: caller is not the owner");
-         fsm.setEjector(tu);
-         vm.stopBroadcast();
+        address[] memory operators = new address[](2);
+        operators[0] = address(0x1);
+        operators[1] = address(0x2);
 
-         address ejectorAddr2 = fsm.ejector();
-         emit log_address(ejectorAddr2);
-         assert(ejectorAddr == ejectorAddr2);
+        bytes[] memory quorumNumbers = new bytes[](2);
+        quorumNumbers[0] = BitmapUtils.bitmapToBytesArray(0);
+        quorumNumbers[1] = BitmapUtils.bitmapToBytesArray(1);
+        console.log("quorumNumbers");
 
-   }
-   function testEjectorCanEject() public {
+        vm.startBroadcast(ejectorAddr);
+        fsm.ejectOperators(operators, quorumNumbers);
+        vm.stopBroadcast();
+    }
 
-         FinalizerServiceManager fsm  = FinalizerServiceManager(stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager"));
-         address ejectorAddr = fsm.ejector();
+    function testOwnerCanEject() public {
+        FinalizerServiceManager fsm = FinalizerServiceManager(
+            stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager")
+        );
+        address ejectorAddr = fsm.owner();
 
-         address[] memory operators = new address[](2);
-            operators[0] = address(0x1);
-            operators[1] = address(0x2);
+        address[] memory operators = new address[](2);
+        operators[0] = address(0x1);
+        operators[1] = address(0x2);
 
-         bytes[] memory quorumNumbers = new bytes[](2) ;
-         quorumNumbers[0] = BitmapUtils.bitmapToBytesArray(0);
-         quorumNumbers[1] = BitmapUtils.bitmapToBytesArray(1);
-         console.log("quorumNumbers");
+        bytes[] memory quorumNumbers = new bytes[](2);
+        quorumNumbers[0] = BitmapUtils.bitmapToBytesArray(0);
+        quorumNumbers[1] = BitmapUtils.bitmapToBytesArray(1);
+        console.log("quorumNumbers");
 
-         vm.startBroadcast(ejectorAddr);
-         fsm.ejectOperators(operators, quorumNumbers);
-         vm.stopBroadcast();
-
-   }
-   function testOwnerCanEject() public {
-
-         FinalizerServiceManager fsm  = FinalizerServiceManager(stdJson.readAddress(new TestUtils().getRollDownConfigFileEth(), ".addresses.serviceManager"));
-         address ejectorAddr = fsm.owner();
-
-         address[] memory operators = new address[](2);
-            operators[0] = address(0x1);
-            operators[1] = address(0x2);
-
-         bytes[] memory quorumNumbers = new bytes[](2) ;
-         quorumNumbers[0] = BitmapUtils.bitmapToBytesArray(0);
-         quorumNumbers[1] = BitmapUtils.bitmapToBytesArray(1);
-         console.log("quorumNumbers");
-
-         vm.startBroadcast(deployer);
-         fsm.ejectOperators(operators, quorumNumbers);
-         vm.stopBroadcast();
-
-   }
+        vm.startBroadcast(deployer);
+        fsm.ejectOperators(operators, quorumNumbers);
+        vm.stopBroadcast();
+    }
 }
