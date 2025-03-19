13d61db34a160d4a5dd0563f7cd809a4cc714a74
Misha Kolesnik <tenequm@gmail.com>
2025-03-19 18:29:25 +0000

    ci: test updated gasp-e2e submodule dependency (#502)
    * chore: set gasp-e2e submodule reference to ea1a9cb9a0c88e8ace55ee93e466c8fa98020587
    
    * fix: remove unused dependency installation
    
    * ci: add possibilit to disable moon cache in e2e tests workflow through `[no cache]` text in commit message [no cache]
    
    * chore: fix e2e tests job disable cache step [no cache]
    
    * fix: testmo report submission step
    
    * fix: no cache step [no cache]
    
    * fix: ensure yarn 4.7.0 is used in e2e-tests workflow [no cache]
    
    * ci: replace commit message cache disabling with checking for `no-cache` PR label to decide if cache should be disabled
    
    * chore: specify more specific inputs to be checked for existing cache when checking cache for e2e-tests with `moon`
    
    * fix: testmo reports directory reference
    
    * chore: set `wait-for-completion-interval` for gasp-node-generate-types job to reduce the time waiting of the remote workflow completion
    
    * chore: bump CACHE_VERSION in reusable e2e-test-new workflow
    
    * chore: update the reference to point to correct e2e tests repo branch
    
    * refactor: streamline caching logic in e2e test workflow
    
    Updated the GitHub Actions workflow to simplify cache management. The `MOON_CACHE` variable is now set to 'off' for the `release/next` branch and when a 'no-cache' label is present on pull requests, removing redundant checks and improving clarity.
    
    * chore: update gasp-e2e repository reference to point back to eth-rollup-develop
    
    * chore: fix contracts formatting
    
    * chore: fix linter issues
    
    * chore: update default branch in workspace configuration from 'main' to 'develop'
    
    * chore: update .gitignore to exclude package-lock.json
    
    Added package-lock.json to .gitignore to ensure consistency in package management, as we are using yarn for our projects. This helps prevent potential conflicts and keeps the repository clean.
    
    * chore: enhance e2e test scripts for better CI visibility
    
    Updated the e2e test scripts in moon.yml to improve error handling and logging. Removed the `set -eo pipefail` command and added conditional checks to output the last 1000 lines of logs when tests fail, ensuring better visibility during CI runs. Also added .giga to .gitignore for cleaner project management.
    
    * fix: use correct path to execute docker compose logs command on failure
    
    * chore: add docker compose ps command for better container visibility
    
    Inserted `docker compose ps` command in multiple locations within moon.yml files to enhance visibility of running containers during e2e test execution. This will aid in debugging and monitoring the state of services during CI runs.
    
    * chore: improve e2e test script for CI environment
    
    Refactored the e2e test scripts in moon.yml to conditionally handle Docker Compose commands based on the CI environment. This change ensures that `docker compose up -d` runs without building images in CI, enhancing efficiency and reducing unnecessary build times. The `set -e` command was commented out for better error handling visibility during test execution.

--- contracts/test/integration/TestUtils.sol
+++ contracts/test/integration/TestUtils.sol
@@ -1,25 +1,25 @@
 pragma solidity =0.8.13;
 
-import "forge-std/Test.sol";
-import "forge-std/Script.sol";
-import "forge-std/StdJson.sol";
-import "forge-std/console.sol";
+import {Script} from "forge-std/Script.sol";
+import {Test} from "forge-std/Test.sol";
 
 contract TestUtils is Script, Test {
-
-    function getAvsConfigFile()  view public returns (string memory){
+    function getAvsConfigFile() public view returns (string memory) {
         string memory configData = vm.readFile("script/output/31337/avs_deployment_output.json");
         return configData;
     }
-    function getRollDownConfigFileEth()  view public returns (string memory){
+
+    function getRollDownConfigFileEth() public view returns (string memory) {
         string memory configData = vm.readFile("script/output/31337/rolldown_output.json");
         return configData;
     }
-    function getRollDownConfigFileArb()  view public returns (string memory){
+
+    function getRollDownConfigFileArb() public view returns (string memory) {
         string memory configData = vm.readFile("script/output/31337/arbitrum_rolldown_output.json");
         return configData;
     }
-    function getErc20ConfigFile() view public returns (string memory){
+
+    function getErc20ConfigFile() public view returns (string memory) {
         string memory configData = vm.readFile("script/output/31337/strategy_output.json");
         return configData;
     }
